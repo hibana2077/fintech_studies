c2d331fbf498848ddb448084f11e4023c5f2e003
hibana2077 <hibana2077@gmail.com>
2023-07-04 21:50:13 +0800

    add commsion

--- ai_tpsl/src/env.py
+++ ai_tpsl/src/env.py
@@ -1,11 +1,4 @@
-'''
-Author: hibana2077 hibana2077@gmail.com
-Date: 2023-06-25 19:05:09
-LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2023-07-04 21:43:32
-FilePath: \fintech_studies\ai_tpsl\src\env.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 import gym
 from gym import spaces
 import numpy as np
@@ -13,10 +6,11 @@ import numpy as np
 class StockTradingEnv(gym.Env):
     metadata = {'render.modes': ['human']}
   
-    def __init__(self, df):
+    def __init__(self, df, commission=0.0001):
         super(StockTradingEnv, self).__init__()
 
         self.df = df
+        self.commsion = commission
         self.reward_range = (0, np.inf)
         self.action_space = spaces.Discrete(3)  # buy, sell, hold
         self.observation_space = spaces.Box(low=0, high=np.inf, shape=(6,))
