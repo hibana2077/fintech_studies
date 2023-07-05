03a67310ca82558fa58d157707173389525b9f4d
hibana2077 <hibana2077@gmail.com>
2023-07-05 09:54:39 +0800

    update env

--- ai_tpsl/src/env.py
+++ ai_tpsl/src/env.py
@@ -1,3 +1,11 @@
+'''
+Author: hibana2077 hibana2077@gmail.com
+Date: 2023-06-25 19:05:09
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2023-07-05 09:54:31
+FilePath: \fintech_studies\ai_tpsl\src\env.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 
 import gym
 from gym import spaces
