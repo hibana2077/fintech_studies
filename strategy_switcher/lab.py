7be17246aeb577ad0af9e537e4f932117872e8f8
hibana2077 <hibana2077@gmail.com>
2023-10-20 10:39:01 +0800

    update

--- strategy_switcher/lab.py
+++ strategy_switcher/lab.py
@@ -1,2 +1,18 @@
-a = "time,open,high,low,close,volume,ema_22,ema_50,ema_200,rsi,kurtosis,mad,tos_stdevall,signal1,signal2"
-print(len(a.split(',')))
+'''
+Author: hibana2077 hibana2077@gmail.com
+Date: 2023-10-05 13:35:37
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2023-10-19 18:12:37
+FilePath: \fintech_studies\strategy_switcher\lab.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import numpy as np
+
+#generate array
+
+a = np.random.randn(5,10)
+
+print(a)
+
+b = np.arange(-0.2,1.8,0.1)
+print(b)
